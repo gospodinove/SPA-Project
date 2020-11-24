550a3ce8f94b1eeaa63bbbd9302de6d2b56d384f
Emanuil Gospodinov <emanuil.gospodinov@trading212.com>
2020-11-24 18:25:40 +0200

    Initial Commit

---
+++ SPA Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPA Project
+//
+//  Created by Emanuil Gospodinov on 24.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
